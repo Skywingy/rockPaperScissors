9ad9eab503010a35c144133b6856ef919f91dbe7
Nyam-Erdene N <Nyamka523@gmail.com>
2022-06-19 13:34:35 +0800

    Well, clear clear q clear > clear git commit -m Well I have completed it. > sadfaSD> Q CLEAR git add pseudocode.txt clear dquote> cd

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # rockPaperScissors
+
+HELLO, Today we are going to create Rock, Paper, Scissors...
+
+And I have some idea how to frame it. Let's see how far we can go.
--- java.js
+++ java.js
@@ -1,7 +1,62 @@
+//Random selection array
+const select = ["rock", "paper", "scissors"];
+let playerSelectionCount = 0;
+let computerSelectionCount = 0;
+//Random selection function
 function computerPlay() {
-const options = ["Rock", "Paper", "Scissors"];
-const random = Math.floor(Math.random() * options.length);
-alert(options[random]);
+    const random = select[Math.floor(Math.random() * select.length)];
+    return random;
 }
 
-computerPlay();
+//Com and Player selection phase and winner
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
+    console.log("player:", playerSelection)
+    computerSelection = computerPlay() ;
+    console.log("com:", computerSelection);
+    if (playerSelection == "rock" && computerSelection == "scissors" ||
+        playerSelection == "paper" && computerSelection == "rock" ||
+        playerSelection == "scissors" && computerSelection == "paper") {
+            playerSelectionCount++;
+            console.log("Yeey you won!!! GOODJOB!");
+        }
+    else if (playerSelection == "rock" && computerSelection == "paper" ||
+             playerSelection == "paper" && computerSelection == "scissors" ||
+             playerSelection == "scissors" && computerSelection == "rock"){
+                 computerSelectionCount++;
+                 console.log("It's okay you will win next time! Cheer UP!");
+             }
+    else if (playerSelection == "rock" && computerSelection == "rock" ||
+            playerSelection == "paper" && computerSelection == "paper" ||
+            playerSelection == "scissors" && computerSelection == "scissors"){
+                console.log("Goodgame! You tie! Shake Hands!");
+            }
+    else {
+        console.log("Please type Rock, Paper or Scissors!");
+    }
+    console.log(playerSelectionCount);
+    console.log(computerSelectionCount);
+}
+
+
+//5 round play
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        playRound();
+    }
+    if (playerSelectionCount > computerSelectionCount) {
+        console.log("Ok you're solid one! Overall you won!")
+    }
+    else if (playerSelectionCount == computerSelectionCount) {
+        console.log("Well... Tie it's")
+    }
+    else {
+        console.log("What can we say... I know it's hard to lose. But hey chin up!! Try better next time!")
+    }
+}
+game();
+
+
+    
